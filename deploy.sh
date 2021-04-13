6112d56459544f1397d899a744730838e04efad7
makaiqi <1358328279@qq.com>
2021-04-13 14:28:14 +0800

    提交

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,4 @@ git push -f git@github.com:makaiqi/makaiqi.github.io.git master
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
 
-cd -
+#cd -
